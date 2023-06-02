94c99e97e99f62c296a4d1ca6858572bf618e27c
Anbar Saleem <45743448+anbarsaleem@users.noreply.github.com>
2023-06-01 23:14:08 -0400

    completed basic backend to frontend connection
    Was able to connect express based api to vue based client for the first time from diff ports.

---
+++ app/client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p class="App-intro">{{ apiResponse }}</p>
+    <h1>HI</h1>
+  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script>
+export default {
+  data() {
+    console.log("retrieving data");
+    return {
+      apiResponse: "",
+    };
+  },
+  created() {
+    this.callAPI();
+    console.log("called api");
+  },
+  methods: {
+    callAPI() {
+      fetch("http://localhost:9000/testAPI")
+        .then((res) => res.text())
+        .then((res) => (this.apiResponse = res));
+      console.log("fetched api response");
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
