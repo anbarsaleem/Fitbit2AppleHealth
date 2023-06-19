807d9262559e3be5bc87e8008b3fbc87bf33719b
Anbar Saleem <45743448+anbarsaleem@users.noreply.github.com>
2023-06-19 03:14:46 -0400

    Update fitbitAuth.js

--- app/api/routes/fitbitAuth.js
+++ app/api/routes/fitbitAuth.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+require(dotenv).config();
+
+const serverUrl = process.env.SERVER_URL;
+const formattedUrl = encodeURIComponent(serverUrl);
 
 /* GET fitbitAuth page. */
 router.get('/', function(req, res, next) {
-  res.send("hhttps://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23QYBD&scope=activity+cardio_fitness+electrocardiogram+heartrate+location+nutrition+oxygen_saturation+profile+respiratory_rate+settings+sleep+social+temperature+weight&redirect_uri=https%3A%2F%2Fmurmuring-temple-36333-5270dc63cfd6.herokuapp.com%2FfitbitAuthCallback")
+  res.send("hhttps://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23QYBD&scope=activity+cardio_fitness+electrocardiogram+heartrate+location+nutrition+oxygen_saturation+profile+respiratory_rate+settings+sleep+social+temperature+weight&redirect_uri=" + formattedUrl + "fitbitAuthCallback");
 });
 
 module.exports = router;
