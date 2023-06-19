866adb9b4e1746252189a473cf5045ac9d81c918
Anbar Saleem <45743448+anbarsaleem@users.noreply.github.com>
2023-06-19 02:33:28 -0400

    deploy configuration for backend

--- app/api/routes/fitbitAuth.js
+++ app/api/routes/fitbitAuth.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET fitbitAuth page. */
 router.get('/', function(req, res, next) {
-  res.send("https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23QYBD&scope=activity+cardio_fitness+electrocardiogram+heartrate+location+nutrition+oxygen_saturation+profile+respiratory_rate+settings+sleep+social+temperature+weight&redirect_uri=http%3A%2F%2Flocalhost%3A9000%2FfitbitAuthCallback")
+  res.send("hhttps://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23QYBD&scope=activity+cardio_fitness+electrocardiogram+heartrate+location+nutrition+oxygen_saturation+profile+respiratory_rate+settings+sleep+social+temperature+weight&redirect_uri=https%3A%2F%2Fmurmuring-temple-36333-5270dc63cfd6.herokuapp.com%2FfitbitAuthCallback")
 });
 
 module.exports = router;
